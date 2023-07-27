8bb994b18e48e73fb7eec1ceac07eb2c26cbeb6c
Charllys Fernandes <charllys.daiany@gmail.com>
2023-07-26 23:26:13 -0300

    Add automatic search for .jwlibrary files in the executable folder
    Modify the Main method to automatically search for all .jwlibrary files in the root directory of the executable instead of requiring command-line arguments. This change allows the program to merge the found files without the need for additional input from the user.

--- JWLSLMerge/Program.cs
+++ JWLSLMerge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using JWLSLMerge.Data;
+using System;
+using System.IO;
 
 namespace JWLSLMerge
 {
@@ -6,15 +8,21 @@ namespace JWLSLMerge
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            // Obtém o caminho da pasta do executável
+            string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Obtém todos os arquivos .jwlibrary na pasta do executável
+            string[] jwlibraryFiles = Directory.GetFiles(executablePath, "*.jwlibrary");
+
+            if (jwlibraryFiles.Length < 2)
             {
-                Console.WriteLine("Please provide at least two .jwlibrary files with their full paths.");
+                Console.WriteLine("Please make sure there are at least two .jwlibrary files in the executable folder.");
                 return;
             }
 
             MergeService mergeService = new MergeService();
             mergeService.Message += MergeService_Message;
-            mergeService.Run(args);
+            mergeService.Run(jwlibraryFiles);
         }
 
         private static void MergeService_Message(object? sender, string e)
